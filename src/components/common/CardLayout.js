8b0ada1ab703abda7043014dc169eebb5123868b
NotFarFromBlonded <majumdardebashish@ymail.com>
2020-07-06 23:43:35 +0530

    modified

--- src/components/common/CardLayout.js
+++ src/components/common/CardLayout.js
@@ -6,7 +6,7 @@ class CardLayout extends Component{
         return (
             <div id = "blog-card" className="blog-card">
                 <div id = "card-content" className="card-content">
-                    <h1><Link to = {this.props.link} target="_blank" style={{cursor:this.props.cursor}}>{this.props.title}</Link></h1>
+                    <h1><a href = {this.props.link} target="_blank" style={{cursor:this.props.cursor}}>{this.props.title}</a></h1>
                     <p>{this.props.description}</p>
                 </div>
             </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -10,7 +10,7 @@ class Contact extends Component {
             <div id = "contact" className = "contact">
                 <div id = "contact-container" className="contact-container">
                     <div id = "contact-view" className = "contact-view">
-                        <h1><Link to="mailto:majumdardebashish@ymail.com">Mail Me</Link></h1>
+                        <h1><a href="mailto:majumdardebashish@ymail.com">Mail Me</a></h1>
                         <h6>Made with &nbsp;<a href="https://www.youtube.com/watch?v=aHylrIIdX5Q" target="_blank" rel="noopener noreferrer"><i class="fa fa-heart" style = {{fontSize: "12px"}}></i></a>&nbsp; by Debashish Majumdar</h6>
                     </div>  
                 </div>
